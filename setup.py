35c7b06cc1975be9195827db0c93e932851fd977
Helvecio Neto <helvecio.neto@inpe.br>
2025-01-27 09:57:42 -0300

    Update setup version 2.0.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="goesgcp",
-    version='2.0.2',
+    version='2.0.3',
     author="Helvecio B. L. Neto",
     author_email="helvecioblneto@gmail.com",
     description="A package to download and process GOES-16/17 data",
