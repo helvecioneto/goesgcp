a659aef760a70a0ba8c9f31e2b39b6024b479450
Helvecio Neto <helvecio.neto@inpe.br>
2025-02-24 20:23:31 -0300

    Add flags to install gdal using pip

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def install_gdal():
     if system == "Linux":
         print("Instalando GDAL no Linux...")
         subprocess.run(
-            "pip install gdal -f https://girder.github.io/large_image_wheels", 
+            "python -m pip install gdal -f https://girder.github.io/large_image_wheels", 
             shell=True, check=True
         )
 
@@ -39,7 +39,7 @@ def install_gdal():
                 sys.exit(1)
 
             print(f"Downloading Gdal (Python {python_version[0]}.{python_version[1]})...")
-            subprocess.run(f"python3 -m pip install {gdal_url}", shell=True, check=True)
+            subprocess.run(f"python -m pip install {gdal_url}", shell=True, check=True)
 
         else:
             print("Python version not supported for GDAL download.")
